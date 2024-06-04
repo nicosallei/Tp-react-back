dd8fb927c5881bba4ca637f764470d5304c2d9df
Sallei <nicosallei@gmail.com>
2024-06-04 20:54:35 -0300

    arreglando

--- src/main/java/tp/react/back/tpreactback/services/ImagenService.java
+++ src/main/java/tp/react/back/tpreactback/services/ImagenService.java
@@ -26,6 +26,6 @@ public class ImagenService {
         }
 
         imageFile.transferTo(destinationFile);
-        return destinationFile.toString();
+        return imageFile.getOriginalFilename();
     }
 }
